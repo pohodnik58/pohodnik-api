3726838f3ef920e253d88fb39368ea019efa898d
pohodnik58 <pohodnik58@gmail.com>
2020-03-18 20:35:29 +0300

    It works

--- www/pohodnik.tk/connect.php
+++ www/pohodnik.tk/connect.php
@@ -1,9 +1,15 @@
 <?php 
+
+$host = getenv('MYSQL_HOST');
+$user = getenv('MYSQL_USER');
+$psw = getenv('MYSQL_PASSWORD');
+$db = getenv('MYSQL_DATABASE');
+
 $host = 'mysql';
-$conn = mysqli_connect($host, 'poh', 'poh');
+$conn = mysqli_connect($host, $user, $psw, $db);
 if (!$conn) {
     exit('Connection failed: '.mysqli_connect_error().PHP_EOL);
 }
  
-echo 'Successful database connection!'.PHP_EOL;
+echo "Successful database connection {$db}!".PHP_EOL;
  ?>
