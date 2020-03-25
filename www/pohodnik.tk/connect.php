5a9828eb6f8d94d71a12062384a4fcdad0668ee4
pohodnik58 <pohodnik58@gmail.com>
2020-03-25 22:05:37 +0300

    cors fix

--- www/pohodnik.tk/connect.php
+++ www/pohodnik.tk/connect.php
@@ -5,7 +5,6 @@ $user = getenv('MYSQL_USER');
 $psw = getenv('MYSQL_PASSWORD');
 $db = getenv('MYSQL_DATABASE');
 
-$host = 'mysql';
 $conn = mysqli_connect($host, $user, $psw, $db);
 if (!$conn) {
     exit('Connection failed: '.mysqli_connect_error().PHP_EOL);
