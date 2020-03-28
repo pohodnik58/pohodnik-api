f984f80c61f2f3943f95c1a04590e7885ce0f031
pohodnik58 <pohodnik58@gmail.com>
2020-03-28 21:13:27 +0300

    bat scripts

---
+++ start.bat
@@ -0,0 +1,4 @@
+echo off
+cls
+echo start pohodnik-api
+docker-compose up
---
+++ startHosts_ONCE.bat
@@ -0,0 +1,17 @@
+@echo off
+cls
+
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    ECHO Administrator PRIVILEGES Detected! 
+) ELSE (
+    ECHO NOT AN ADMIN!
+	pause
+	exit
+)
+
+echo start change hosts file
+echo %NEWLINE%^127.0.0.1 pohodnik.tk>>%WINDIR%\System32\drivers\etc\hosts
+color 0a
+echo hosts file changed successfuly
+@pause
