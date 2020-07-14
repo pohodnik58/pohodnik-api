9145a3000c7be5f6ff190efd7e17c0ae4877b7f0
Elena <ugoljok91@gmail.com>
2020-07-15 01:49:22 +0300

    поправили удаление

--- www/pohodnik.tk/ajax/medicaments/medicament_del.php
+++ www/pohodnik.tk/ajax/medicaments/medicament_del.php
@@ -6,18 +6,8 @@ include("../../blocks/global.php");
 
 $id_user = $_COOKIE["user"];
 $id = isset($_POST['id'])?intval($_POST['id']):0;
-$id_hiking = isset($_POST['id_hiking'])?intval($_POST['id_hiking']):0;
 
 if(!($id>0)){die(err("id is undefined"));}
-if(!($id_hiking>0)){die(err("id_hiking is undefined"));}
-
-$q = $mysqli->query("SELECT id FROM hiking WHERE id={$id_hiking} AND id_author = {$id_user} LIMIT 1");
-if($q && $q->num_rows===0){
-	$q = $mysqli->query("SELECT id FROM hiking_editors WHERE id_hiking={$id_hiking}  AND is_medic=1  AND id_user = {$id_user} LIMIT 1");
-	if($q && $q->num_rows===0){
-		die(json_encode(array("error"=>"Нет доступа")));
-	}
-}
 
 $z = "DELETE FROM `medicaments` WHERE `id`={$id}";
 $q = $mysqli->query($z);
